3e12a5e25c00f8ed9187bf2129adf00f79284c0f
Mohammad Saleh <scrolsin333@gmail.com>
2023-12-28 12:09:18 +0330

    update user panel

--- app/Http/Controllers/user/UserController.php
+++ app/Http/Controllers/user/UserController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\user;
 use App\Http\Controllers\Controller;
 use App\Models\Comment;
 use App\Models\Post;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
@@ -15,12 +16,16 @@ class UserController extends Controller
         $posts = count(\App\Models\User::find(auth()->user()->id)->posts);
         $comments = count(\App\Models\User::find(auth()->user()->id)->comments);
 
-        $yposts = Post::paginate(2);
+        $user = User::find(auth()->user()->id);
 
+        $yposts = $user->posts()->paginate(8);
+        $ycomments = $user->comments()->paginate(8);
+        
         return view("user.panel" , [
             "posts" => $posts,
             "comments" => $comments,
             "items" => $yposts,
+            "items2" => $ycomments,
         ]);
     }
 }
--- resources/views/user/panel.blade.php
+++ resources/views/user/panel.blade.php
@@ -36,20 +36,66 @@
 
     <div class="menu">
       <ul class="list-group list-group-flush">
-        @foreach($items as $item)
-            <li class="list-group-item model_btn">
-                    <h3 class="item-title">{{ $item->title }}</h3>
-                    <div class="space-x"></div>
 
+		@if ($posts == 0)
+		<li class="list-group-item model_btn">
+			<h3 class="item-title">You have not left any posts</h3>
+			
+		</li>
+		@else
+			@foreach($items as $item)
+			<li class="list-group-item model_btn vfl">
+					<h3 class="item-title">{{ $item->title }}</h3>
+					<div class="space-x"></div>
+					<a class="btn btn-link">Sea more</a>
+
+
+
+			</li>
+			@endforeach
+
+			<div class="mau">
+				{{ $items->links() }}
+			</div>
+		@endif
+
+        
+        
+    </ul>
+
+    <div class="vfl mau menu">
+      <div class="">List Your Comment</div>
+      
+    </div>
+
+    <div class="menu">
+      <ul class="list-group list-group-flush">
+
+        @if ($comments == 0)
+		<li class="list-group-item model_btn">
+			<h3 class="item-title">You have not left any comments</h3>
+			
+		</li>
+        @else
+          @foreach($items2 as $item2)
+            <li class="list-group-item model_btn vfl">
+                    <h3 class="item-title">{{ substr($item2->comment, 0 ,30)."...." }}</h3>
+                    <div class="space-x"></div>
+					<a class="btn btn-link">Sea more</a>
 
 
 
             </li>
-        @endforeach
+          @endforeach
 
-        <div class="mau">
-            {{ $items->links() }}
-        </div>
+		  <div class="mau">
+            {{ $items2->links() }}
+          </div>
+        @endif
+
+        
+
+        
         
     </ul>
 
